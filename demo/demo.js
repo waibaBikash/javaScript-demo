a8f7f5763a0bd5149c57341cd77e0a30d28193ad
waibaBikash <kavrelibikash@gmail.com>
2024-04-21 14:53:00 +1000

    using function toUppercase

--- demo/demo.js
+++ demo/demo.js
@@ -1,1 +1,10 @@
-console.log(prompt("Compose your tweet").slice(0,120));
+// # using the funtion toUpperCase and toLowwerCase 
+let  name = prompt('Enter Your name');
+let   firstChar = name.slice(0,1);
+
+let upperCasefirstChar = firstChar.toUpperCase();
+let restOfName = name.slice(1,name.length);
+let restOfNameLowwercase = restOfName.toLowerCase(restOfName);
+ let finalResult = upperCasefirstChar + restOfNameLowwercase;
+
+ alert('hello ' + finalResult);
